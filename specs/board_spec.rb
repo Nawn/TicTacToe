f6cc61031e4dd66d064c4e8b693b813960423f5a
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-01-06 22:43:51 -0800

    Add tests to get program running

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -1,5 +1,22 @@
 require_relative 'spec_helper'
 
 describe Board do
+
+  before(:each) do
+    rows = [["", "", ""], :separator, ["", "", ""], :separator, ["", "", ""]]
+    @empty_table = Terminal::Table.new :rows => rows
+    @board = Board.new
+  end
   
+  describe "#new" do    
+    it "creates an empty board" do
+      expect(@board.table).to eql(@empty_table)
+    end
+  end
+  
+  describe "#display" do 
+    it "displays(puts) the table onto command prompt" do
+      
+    end
+  end  
 end
--- specs/player_spec.rb
+++ specs/player_spec.rb
@@ -1,5 +1,26 @@
 require_relative 'spec_helper'
 
 describe Player do
-  
+  describe "#new" do
+    before(:all) do
+      @player = Player.new ("X")
+    end
+    
+    it "creates an instance of Player" do
+      expect(@player).to be_a Player
+    end
+    
+    context "when given input letter, X" do
+      it "will have an input letter of X" do
+        expect(@player.input_letter).to eql("X")
+      end
+    end
+    
+    context "when given input letter, O" do
+      it "will have an input letter of O" do
+        @player = Player.new("O")
+        expect(@player.input_letter).to eql("O")
+      end
+    end
+  end
 end
