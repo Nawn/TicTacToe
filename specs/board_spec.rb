abba563754f680f0e0a6ad849503f28f88768367
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-01-07 23:04:05 -0800

    Adding more tests for the Board class

--- specs/board_spec.rb
+++ specs/board_spec.rb
@@ -14,9 +14,38 @@ describe Board do
     end
   end
   
-  describe "#display" do 
-    it "displays(puts) the table onto command prompt" do
+  describe "#input" do
+    context "when given no/wrong arguments" do
+      it "raises errror" do
+        expect(@board.input()).to raise_error
+        expect(@board.input("Top one")).to raise_error
+        expect(@board.input(" ")).to raise_error
+      end
+    end
+    
+    context "when given an empty spot" do
+      it "will attach the given inputLetter to the appropriate spot" do
+        
+      end
+    end
+    
+    context "when given a used block" do
+      it "will raise an error" do
+        
+      end
+    end
+  end
+  describe "#done?" do
+    context "when there are no lines done" do
       
     end
-  end  
+    
+    context "when there's a tie/no more room" do
+      
+    end
+    
+    context "when there are some matches" do
+      
+    end
+  end
 end
