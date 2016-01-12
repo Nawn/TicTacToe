669e35b3076c3b3bd27fb777c668a81713b8aef8
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-01-11 23:28:31 -0800

    got a Temp board displaying for now. Need to refactor code for Rows layout/design

--- board.rb
+++ board.rb
@@ -1,8 +1,18 @@
 require 'terminal-table'
 
 class Board
-@@new_game = {A: ["", "", ""], B: ["", "", ""], C: ["", "", ""]}
+attr_accessor :table
+attr_accessor :rows
+@@new_game = [["A", nil, nil, nil], :separator, ["B", nil, nil, nil], :separator, ["C", nil, nil, nil]]
+
   def initialize (input_rows=@@new_game)
     @rows =  input_rows
+    @table = Terminal::Table.new :headings => ['0-0', 1, 2, 3], :rows => @rows
+    
+    puts @table
   end  
+  
+  def display
+    puts @table
+  end
 end
