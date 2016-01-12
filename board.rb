9e0b3bd289d3c197ba6b8fab40462748aa1da38a
Raul Alejandro Gonzalez <Nawn1994@gmail.com>
2016-01-12 13:02:43 -0800

    Switched the layout of the rows setup. Uses Hash now

--- board.rb
+++ board.rb
@@ -3,11 +3,14 @@ require 'terminal-table'
 class Board
 attr_accessor :table
 attr_accessor :rows
-@@new_game = [["A", nil, nil, nil], :separator, ["B", nil, nil, nil], :separator, ["C", nil, nil, nil]]
+@@new_game = {:a => {1 => nil, 2 => nil, 3 => nil}, :b => {1 => nil, 2 => nil, 3 => nil}, :c => {1 => nil, 2 => nil, 3 => nil}}
 
   def initialize (input_rows=@@new_game)
     @rows =  input_rows
-    @table = Terminal::Table.new :headings => ['0-0', 1, 2, 3], :rows => @rows
+    
+    table_array = [[@rows[:a][1], @rows[:a][2], @rows[:a][3]], :separator, [@rows[:b][1], @rows[:b][2], @rows[:c][3]], :separator, [@rows[:c][1], @rows[:c][2], @rows[:c][3]]]
+    
+    @table = Terminal::Table.new :headings => ['0-0', 1, 2, 3], :rows => table_array
     
     puts @table
   end  
